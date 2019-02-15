f3925631ac9b1fba8f4c661d6a31199711a2b28a
Garrett Weems <gwgraphicdesign@gmail.com>
2019-02-15 18:59:44 -0500

    TodoItem Finished

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
+import "../index.scss";
 class TodoItem extends Component {
   getStyle = () => {
     return {
-      background: "#f4f4f4",
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
+      width: "100%",
+      display: "flex",
+      flexDirection: "row",
+      justifyContents: "center",
+      fontStyle: this.props.todo.completed ? "italic" : "normal",
+      color: this.props.todo.completed ? "grey" : "black",
       textDecoration: this.props.todo.completed ? "line-through" : "none"
     };
   };
@@ -14,39 +17,37 @@ class TodoItem extends Component {
   render() {
     const { id, title } = this.props.todo;
     return (
-      <div style={this.getStyle()}>
-        <p>
+      <li className="todo-item" style={this.getStyle()}>
+        <div className="checkbox">
           <input
             type="checkbox"
             onChange={this.props.markComplete.bind(this, id)}
-          />{" "}
-          {title}
-          <svg
-            viewBox="0 0 40 40"
-            style={svgStyle}
-            className="button is-small is-danger is-rounded"
-            onClick={this.props.delTodo.bind(this, id)}
-          >
-            <path d="M 10,10 L 30,30 M 30,10 L 10,30" />
-          </svg>
-        </p>
-      </div>
+          />
+          <label>{title}</label>
+        </div>
+        <svg
+          viewBox="0 0 40 40"
+          onClick={this.props.delTodo.bind(this, id)}
+          style={svgStyle}
+        >
+          <path d="M 10,10 L 30,30 M 30,10 L 10,30" />
+        </svg>
+      </li>
     );
   }
 }
 
 const svgStyle = {
-  float: "right",
-  width: "60px",
-  stroke: "black",
-  strokeWidth: "10"
+  width: "1.5rem",
+  stroke: "hsl(348, 100%, 61%)",
+  strokeWidth: "7"
 };
 
 // PropTypes
 TodoItem.propTypes = {
-  todo: PropTypes.object.isRequired
-  // markComplete: PropTypes.func.isRequired,
-  // delTodo: PropTypes.func.isRequired,
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 };
 
 export default TodoItem;

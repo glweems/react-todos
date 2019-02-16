60b43731e51e7e9b5a68afb47583cfbcb71809c4
Garrett Weems <gwgraphicdesign@gmail.com>
2019-02-15 19:00:48 -0500

    Header Finished, background based on prop.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
+import Navbar from "./Navbar";
 export class Header extends Component {
   render() {
     return (
-      <section className={headerClasses()}>
-        <div className="hero-body">
-          <h1 className="title">Todos</h1>
-        </div>
-      </section>
+      <React.Fragment>
+        <section className={headerClasses() + " " + this.props.background}>
+          <div className="hero-body">
+            <h1 className="title">{this.props.title}</h1>
+          </div>
+        </section>
+        <Navbar />
+      </React.Fragment>
     );
   }
 }
 
-const headerClasses = () => ["hero is-primary"];
+const headerClasses = () => ["hero container"];
+
+// PropTypes
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Header;
